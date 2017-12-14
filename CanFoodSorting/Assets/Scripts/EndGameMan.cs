4e3d75aa3252a80607f54447b6285dac61e4f37d
Aristotelis Papaioannou <adpapaio@umich.edu>
2017-12-14 03:04:18 -0500

    Lots of Changes, two more things until done
    Added:
    Main Menu
    End Menu
    Highscore
    Text Mesh Pro
    Timer
    Roof
    
    Fixed:
    Lighting
    
    Needed:
    Sounds
    Banners above boxes
    
    ReTest all new features on WEBGL and Android

---
+++ CanFoodSorting/Assets/Scripts/EndGameMan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameMan : MonoBehaviour {
+
+    public TMP_Text finalScore;
+    public TMP_Text bestScore;
+    public Button PlayAgain;
+    public Button MainMenu;
+	// Use this for initialization
+	void Start ()
+    {
+        PlayAgain.onClick.AddListener(() => LoadGame());
+        MainMenu.onClick.AddListener(() => LoadMainMenu());
+        finalScore.text = "FINAL SCORE: " + PlayerPrefs.GetInt("currScore");
+        bestScore.text = "BEST: " + PlayerPrefs.GetInt("highscore");
+    }
+	
+	public void LoadGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- CanFoodSorting/Assets/Scripts/GameMan.cs
+++ CanFoodSorting/Assets/Scripts/GameMan.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 namespace Scoring
 {
@@ -11,30 +13,90 @@ namespace Scoring
         public int currScore;
         public int streakMult;
         public int highScore;
-        public Text scoreText;
-        public Text streakText;
+        public string highscoreKey;
+        public string currscoreKey;
+        public TMP_Text scoreText;
+        public TMP_Text streakText;
+        public TMP_Text highscoreText;
+        public TMP_Text timeLeftText;
         public float timeLeft;
+        bool highscoreExists;
+        private bool redFont;
+        private bool flashing;
+
         // Use this for initialization
         void Start()
         {
-            timeLeft = 2f;
+            redFont = false;
+            flashing = false;
+            timeLeftText.color = new Color32(0, 0, 0, 255);
+            timeLeft = 120f;
             currScore += 0;
             streakMult = 1;
-            scoreText.text = "Score: " + currScore;
-            streakText.text = "Multiplier: X " + streakMult;
+            scoreText.text = "SCORE: " + currScore;
+            streakText.text = "MULTIPLIER: X " + streakMult;
+            highscoreKey = "highscore";
+            currscoreKey = "currScore";
+            highscoreExists = PlayerPrefs.HasKey(highscoreKey);
+            highScore = 0;
+            PlayerPrefs.SetInt(currscoreKey, 0);
+
+            if (highscoreExists)
+            {
+                highScore = PlayerPrefs.GetInt(highscoreKey);
+
+                if (highScore < currScore)
+                {
+                    PlayerPrefs.SetInt(highscoreKey, currScore);
+                    highScore = currScore;
+
+                    PlayerPrefs.Save();
+                }
+                highscoreText.text = "HIGHSCORE: " + highScore.ToString();
+            }
+            else
+            {
+                PlayerPrefs.SetInt(highscoreKey, 0);
+                highscoreText.text = "HIGHSCORE: " + highScore.ToString();
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            timeLeft -= Time.deltaTime;
+            timeLeftText.text = "TIME LEFT: " + timeLeft.ToString("F1") + " secs";
 
+            if (timeLeft <= 0)
+            {
+                EndGame();
+            }
+            else if (timeLeft <= 30f && redFont == false)
+            {
+                redFont = true;
+                timeLeftText.color = new Color32(255, 0, 0, 255);
+            }
+
+            if(timeLeft<=10f && flashing == false)
+            {
+                flashing = true;
+                StartCoroutine(FlashingText());
+            }
+
+            if (highScore < currScore)
+            {
+                PlayerPrefs.SetInt(highscoreKey, currScore);
+                highScore = currScore;
+                highscoreText.text = "HIGHSCORE: " + highScore.ToString();
+                PlayerPrefs.Save();
+            }
         }
 
         public void UpdateScore(bool shouldGive)
         {
             if (shouldGive == true)
             {
-                currScore += 100*streakMult;
+                currScore += 100 * streakMult;
                 streakMult++;
 
             }
@@ -44,15 +106,42 @@ namespace Scoring
                 streakMult = 1;
             }
 
-            scoreText.text = "Score: " + currScore;
-            streakText.text = "Multiplier: X " + streakMult;
+            scoreText.text = "SCORE: " + currScore;
+            streakText.text = "MULTIPLIER: X " + streakMult;
 
         }
 
         public void Missed()
         {
             streakMult = 1;
-            streakText.text = "Multiplier: X " + streakMult;
+            streakText.text = "MULTIPLIER: X " + streakMult;
+        }
+
+        public void EndGame()
+        {
+            if (highScore < currScore)
+            {
+                PlayerPrefs.SetInt(highscoreKey, currScore);
+                highScore = currScore;
+                highscoreText.text = "HIGHSCORE: " + highScore.ToString();
+
+            }
+            PlayerPrefs.SetInt(currscoreKey, currScore);
+            PlayerPrefs.Save();
+
+            SceneManager.LoadScene("EndGame");
+        }
+
+        //Blinking Text
+        public IEnumerator FlashingText()
+        {
+            while (true)
+            {
+                timeLeftText.gameObject.SetActive(false);
+                yield return new WaitForSeconds(.35f);
+                timeLeftText.gameObject.SetActive(true);
+                yield return new WaitForSeconds(.35f);
+            }
         }
     }
 }
